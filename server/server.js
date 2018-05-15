be8b249405b663a1cbfa1d0ca2e25bb4be6a0e74
aandre100 <andre-cardoso@outlook.com>
2018-05-15 10:20:20 +0100

    get route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,13 @@ app.post('/todos', (req, res) => {
   });
 
 })
+app.get('/todos', (req, res) => {
+  Todo.find().then((doc) =>{
+    res.send({doc});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
 
 app.listen(3000, () =>{
   console.log("server running on port 3000!!!")
